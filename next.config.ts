c95927de279b5b47a35a1ae762aa7927261d75db
David F <62121330+davidfrnks7@users.noreply.github.com>
2025-09-25 20:27:58 -0400

    Added redirect for root of bio page.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"]
+  },
+  async redirects() {
+    return [
+      {
+        source: "/bio",
+        destination: "/bio/professional",
+        permanent: true
+      }
+    ];
   }
 };
 
