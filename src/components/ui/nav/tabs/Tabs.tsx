eab038e32f59d61172bcc35c2be7843c825389f0
David F <62121330+davidfrnks7@users.noreply.github.com>
2025-09-25 20:27:29 -0400

    Added reusable tabs component.

---
+++ src/components/ui/nav/tabs/Tabs.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { JSX } from "react";
+import { For, Tabs, UseTabsReturn } from "@chakra-ui/react";
+
+export interface TabsObj {
+  title: string;
+  uri: string;
+  // content: string;
+}
+
+export interface TabsProps {
+  tabs: TabsObj[];
+  defaultValue: string;
+  currentValue: UseTabsReturn;
+  title: string;
+}
+
+const TabsNav = ({
+  tabs,
+  defaultValue,
+  currentValue,
+  title
+}: TabsProps): JSX.Element => {
+  const router = useRouter();
+
+  // TODO: Add animation to tabs.
+
+  return (
+    <Tabs.RootProvider
+      key={title}
+      defaultValue={defaultValue}
+      variant={"subtle"}
+      value={currentValue}
+      fitted
+      colorPalette="blue"
+    >
+      <Tabs.List>
+        <For each={tabs}>
+          {({ title, uri }) => (
+            <Tabs.Trigger
+              key={`${title}-${uri}`}
+              value={title}
+              onClick={() => {
+                router.replace(uri);
+              }}
+            >
+              {title}
+            </Tabs.Trigger>
+          )}
+        </For>
+        <Tabs.Indicator />
+      </Tabs.List>
+    </Tabs.RootProvider>
+  );
+};
+
+export default TabsNav;
