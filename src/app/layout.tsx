c5c6c5aeca4f2aa8dce3ad307f276123ae826c36
David F <62121330+davidfrnks7@users.noreply.github.com>
2025-09-26 16:14:42 -0400

    Removed metadata from root layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,6 @@ import { Provider } from "@/components/ui/provider";
 import DesktopNav from "@/components/ui/nav/DesktopNav";
 import { usePathname } from "next/navigation";
 
-export const description =
-  "Mid-level Software Engineer with experience in full-stack development, DevOps, Database Management, PEGA, managing and creating external correspondence, and adhering to client design artifacts/mockups and web accessibility compliance. View my entire list of experience, education and training, and skills.";
-export const title = "David's Portfolio Website";
-
 export default function RootLayout(props: {
   children: React.ReactNode;
 }): JSX.Element {
@@ -20,16 +16,12 @@ export default function RootLayout(props: {
       <meta name="theme-color" content="#3138dc" />
       <link rel="icon" href="/images/favicon.svg" sizes="32x32 192x192" />
       <link rel="apple-touch-icon" href="/images/favicon.svg" />
-      <meta property="og:title" content={title} />
-      <meta name="og:description" content={description} />
       <meta property="og:type" content="Skills and Projects" />
       <meta property="og:image" content="/images/davidFImage.jpg" />
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:alt" content="A face shot of David Franks." />
       <meta property="og:url" content="https://davidfrnks7.dev" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta property="title" content={title} />
-      <meta name="description" content={description} />
       <meta property="type" content="Skills and Projects" />
       <meta property="image" content="/images/davidFImage.jpg" />
       <meta property="image:type" content="image/jpeg" />
