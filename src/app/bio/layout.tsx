b721a767b4c3f96c6c96a7ed4a14d2fae888dacd
David F <62121330+davidfrnks7@users.noreply.github.com>
2025-09-26 14:53:37 -0400

    Stylized component. Added portrait image to page.

--- src/app/bio/layout.tsx
+++ src/app/bio/layout.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { JSX } from "react";
+import Image from "next/image";
 import {
-  Box,
   HStack,
   ScrollArea,
   useTabs,
   VStack,
-  Text
+  Text,
+  Flex,
+  Box
 } from "@chakra-ui/react";
 import TabsNav, { TabsObj } from "@/components/ui/nav/tabs/Tabs";
 import BackgroundImage from "@/components/ui/bgImage";
+import portrait from "../../../public/images/portrait.jpg";
 
 const BioPageNav = (): JSX.Element => {
   const defaultValue = "Professional";
@@ -23,8 +26,17 @@ const BioPageNav = (): JSX.Element => {
     defaultValue: defaultValue
   });
 
+  /**
+   * TODOS:
+   * ! Make page responsive to screen size.
+   * ! Add bios to a file in _lib folder.
+   * ! Remove hard-coded height and width props.
+   * * Import and use the bios file.
+   * * Stylize the component more.
+   */
+
   return (
-    <HStack
+    <Flex
       h="100vh"
       w="100vw"
       justifyContent="center"
@@ -32,26 +44,47 @@ const BioPageNav = (): JSX.Element => {
       alignItems="center"
     >
       <BackgroundImage />
-      <Box h="1000px" w="500px">
-        {"Placeholder image"}
-      </Box>
-      <VStack>
-        <TabsNav
-          tabs={bios}
-          defaultValue={defaultValue}
-          currentValue={tabs}
-          title="bio-tabs"
-        />
-        <Box>
+      <HStack
+        px={4}
+        py={8}
+        rounded="3xl"
+        w="80%"
+        justifyContent="flex-start"
+        alignContent="center"
+        alignItems="center"
+        bg="#09090b80"
+        gap={6}
+      >
+        <Box h="auto" w={{ lg: "30%", xl: "25%" }} m={6}>
+          <Image
+            layout="intrinsic"
+            src={portrait}
+            alt='Picture of David "Leo" Franks'
+            placeholder="blur"
+          />
+        </Box>
+        <VStack
+          justifyContent="flex-start"
+          alignContent="flex-start"
+          alignItems="flex-start"
+          flex={1}
+        >
+          <TabsNav
+            tabs={bios}
+            defaultValue={defaultValue}
+            currentValue={tabs}
+            title="bio-tabs"
+          />
           <ScrollArea.Root
             colorPalette="blue"
             variant="always"
             height="1000px"
             w="100%"
+            bg="#09090bE6"
           >
             <ScrollArea.Viewport>
               <ScrollArea.Content>
-                <Text>{`This will be the content from ${tabs.value}`}</Text>
+                <Text>{`This will be the content from the ${tabs.value} bio`}</Text>
               </ScrollArea.Content>
             </ScrollArea.Viewport>
             <ScrollArea.Scrollbar>
@@ -59,9 +92,9 @@ const BioPageNav = (): JSX.Element => {
             </ScrollArea.Scrollbar>
             <ScrollArea.Corner />
           </ScrollArea.Root>
-        </Box>
-      </VStack>
-    </HStack>
+        </VStack>
+      </HStack>
+    </Flex>
   );
 };
 
