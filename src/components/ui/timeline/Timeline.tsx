5fd97ac436e3d56c49e92c48e48e48a3ce1b91e3
Leo F <62121330+davidfrnks7@users.noreply.github.com>
2025-11-07 11:55:10 -0500

    Adjust animation speed.

--- src/components/ui/timeline/Timeline.tsx
+++ src/components/ui/timeline/Timeline.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Fragment, JSX } from "react";
-import { Timeline, Text, VStack, For, Flex } from "@chakra-ui/react";
+import { Timeline, Text, VStack, For, Flex, Presence } from "@chakra-ui/react";
 import { TimelineArr, TimelineItem } from "@/data/timelines";
 
 interface TimelineProps {
@@ -42,58 +44,68 @@ const TimelineComponent = ({ timeline, icon }: TimelineProps): JSX.Element => {
             const subTitle = course || jobTitle || certIssuer || "";
 
             return (
-              <Timeline.Item
-                width="100%"
+              <Presence
+                present={true}
+                animationStyle={{
+                  _open: "scale-fade-in",
+                  _closed: "scale-fade-out"
+                }}
+                animationDuration="slowest"
                 id={title.replaceAll(" ", "-")}
                 key={title.replaceAll(" ", "-")}
+                width="100%"
               >
-                <Timeline.Connector colorPalette={"blue"}>
-                  <Timeline.Separator />
-                  <Timeline.Indicator fontSize="xl">{icon}</Timeline.Indicator>
-                </Timeline.Connector>
-                <Timeline.Content>
-                  <Timeline.Title as="h2" fontSize="lg" fontWeight="bold">
-                    {title}
-                  </Timeline.Title>
-                  <Flex
-                    w="auto"
-                    justifyContent="space-between"
-                    alignContent="flex-start"
-                    alignItems="flex-start"
-                    direction={{ base: "column", md: "row" }}
-                    gap={{ base: 2, md: 0 }}
-                  >
-                    <Text width="auto" fontSize="lg">
-                      {subTitle}
-                    </Text>
-                    {location || start || end ? (
-                      <Text fontSize="sm" width="auto">
-                        {start
-                          ? typeof start === "number"
-                            ? start
-                            : start.toLocaleDateString("en-US", {
-                                year: "numeric",
-                                month: "long"
-                              })
-                          : null}
-                        {start && end ? " - " : null}
-                        {end
-                          ? typeof end === "number"
-                            ? end
-                            : end.toLocaleDateString("en-US", {
-                                year: "numeric",
-                                month: "long"
-                              })
-                          : null}
-                        {location ? ` ${location}` : null}
+                <Timeline.Item width="100%">
+                  <Timeline.Connector colorPalette={"blue"}>
+                    <Timeline.Separator />
+                    <Timeline.Indicator fontSize="xl">
+                      {icon}
+                    </Timeline.Indicator>
+                  </Timeline.Connector>
+                  <Timeline.Content>
+                    <Timeline.Title as="h2" fontSize="lg" fontWeight="bold">
+                      {title}
+                    </Timeline.Title>
+                    <Flex
+                      w="auto"
+                      justifyContent="space-between"
+                      alignContent="flex-start"
+                      alignItems="flex-start"
+                      direction={{ base: "column", md: "row" }}
+                      gap={{ base: 2, md: 0 }}
+                    >
+                      <Text width="auto" fontSize="lg">
+                        {subTitle}
                       </Text>
-                    ) : (
-                      <Fragment />
-                    )}
-                  </Flex>
-                  <Text fontSize="md">{description}</Text>
-                </Timeline.Content>
-              </Timeline.Item>
+                      {location || start || end ? (
+                        <Text fontSize="sm" width="auto">
+                          {start
+                            ? typeof start === "number"
+                              ? start
+                              : start.toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long"
+                              })
+                            : null}
+                          {start && end ? " - " : null}
+                          {end
+                            ? typeof end === "number"
+                              ? end
+                              : end.toLocaleDateString("en-US", {
+                                year: "numeric",
+                                month: "long"
+                              })
+                            : null}
+                          {location ? ` ${location}` : null}
+                        </Text>
+                      ) : (
+                        <Fragment />
+                      )}
+                    </Flex>
+                    <Text fontSize="md">{description}</Text>
+                  </Timeline.Content>
+                </Timeline.Item>
+              </Presence>
             );
           }}
         </For>
